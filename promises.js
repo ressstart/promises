9c97a5ce2234e82c76f5ae238807bf4993047f1e
FACELESS <33546892+ressstart@users.noreply.github.com>
2019-05-27 12:43:38 +0500

    Update promises.js

--- promises.js
+++ promises.js
@@ -2,21 +2,34 @@ function applyForVisa(documents ) {
 	console.log('Обработка заявления...');
 	let promise = new Promise(function(resolve, reject){
 			setTimeout(function() {
-				Math.random() > .5 ? resolve({}) : reject('В визе отказано, не хватило документов.');
+				Math.random() > 0 ? resolve({}) : reject('В визе отказано, не хватило документов.');
 			}, 2000);
 	});
 	return promise;
 }
 
-function bookHotel(){}
+function bookHotel(visa){
+	console.log(visa);
+	console.log("Бронируем отель!");
+}
 
-function buyTickets(){}
+function buyTickets(){
+	console.log("Покупаем билеты!");
+}
 
 applyForVisa({})
-	.then(
+	.then(visa => {
+		console.info("Виза получена!");
+	return visa;
+	})
+	.then(bookHotel)
+	.then(buyTickets)
+	.catch(error => console.error(error))
+	.then(() => console.log("I'm here yet!"));
+/*	.then(
 		function(visa) {
 			console.info("Виза получена!");
 	},
 	function(reason) {
 			console.error(reason);
-		});
+		}); */
